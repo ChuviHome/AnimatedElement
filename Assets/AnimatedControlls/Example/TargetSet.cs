54c721d29cf70f23593c1dd285f80ad2cba55bbc
Stanislav <chuvi1155@gmail.com>
2021-02-25 14:20:23 +0300

    First commit

---
+++ Assets/AnimatedControlls/Example/TargetSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSet : MonoBehaviour
+{
+    public AnimatedElementEx ae;
+
+    public void SetTarget(AnimatedBehaviour aeb, float time)
+    {
+        if (aeb.mainTransform.localScale != Vector3.zero)
+        {
+            ae.transformation.to_transformPos = aeb.rtr.anchoredPosition;
+            ae.transformation.Restart();
+        }
+    }
+}
